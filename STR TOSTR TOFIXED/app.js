7c69d8171ad298f556ae8ce349e2d52e0e16787d
moise <mose.ns@hotmail.com>
2020-07-31 21:20:46 +0200

    String Method toString method

---
+++ STR TOSTR TOFIXED/app.js
@@ -0,0 +1,11 @@
+//String(), toString(), toFixed, Number(), parseFloat(), parseInt()
+
+//toString - method -> convert a number to a string.
+//String - function -> convert a number to a string.
+
+const numberToString = 500;
+console.log(typeof numberToString);
+const newString = numberToString.toString();
+console.log(typeof newString);
+
+console.log(typeof String(numberToString));
